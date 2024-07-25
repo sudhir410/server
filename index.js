929adc28c306aee93141b7d6e7d3faaafeb242af
sudhir410 <sudhirchoudhary410@gmail.com>
2024-07-25 15:13:04 +0530

    fix

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+    console.log('Hello World');
     res.send('Hello World!');
     }
 );
+
+app.get('/health', (req, res) => {
+    console.log('checking health');
+    res.status(200).send('OK');
+  });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
